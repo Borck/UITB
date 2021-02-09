26a347519f2118cb07e9fe3f11266381ed7d0ad3
Christian Borck <christian.borck@b-tu.de>
2021-02-09 12:25:55 +0100

    [ADDED] digital clock script

---
+++ Common/ClockDigital.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+[ExecuteInEditMode]
+public class ClockDigital : MonoBehaviour {
+  private TextMeshPro _textClock;
+
+
+
+  private void Awake() {
+    _textClock = GetComponent<TextMeshPro>();
+    EditorApplication.update += Update;
+  }
+
+
+
+  private void Update() {
+    var time = DateTime.Now;
+    var hour = LeadingZero(time.Hour);
+    var minute = LeadingZero(time.Minute);
+    var second = LeadingZero(time.Second);
+    _textClock.text = hour + ":" + minute + ":" + second;
+  }
+
+
+
+  private string LeadingZero(int n) => n.ToString().PadLeft(2, '0');
+}
