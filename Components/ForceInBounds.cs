9ea2683b0e885f06b3d5f08b6b241d2e31e2afdb
Christian Borck <christian.borck@b-tu.de>
2021-02-09 11:04:09 +0100

    [ADDED] script to force an game object to stay inside a boundary

---
+++ Components/ForceInBounds.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Assets.UITB.Extensions;
+using UnityEngine;
+
+
+
+public class ForceInBounds : MonoBehaviour {
+  public Collider Bounds;
+
+  private Vector3 _startPos;
+  private Quaternion _startRot;
+  private Vector3 _startSca;
+
+
+
+  // Start is called before the first frame update
+  private void Start() {
+    if (!IsInBounds()) {
+      enabled = false;
+      Console.Error.WriteLine("GameObject must be initially inside bounds to get the reset position");
+    }
+  }
+
+
+
+  public void UpdateStartTransform() => (_startPos, _startRot, _startSca) = transform.GetTRS();
+
+
+
+  private void LateUpdate() {
+    if (!IsInBounds()) {
+      ResetTransform();
+    }
+  }
+
+
+
+  private bool IsInBounds() => Bounds.bounds.Contains(transform.position);
+
+  private void ResetTransform() => transform.SetTRS(_startPos, _startRot, _startSca);
+}
