4f63ee9518391b709641e00da39335a5723152e3
Borck <borck@hotmail.de>
2021-08-24 14:18:23 +0200

    [BUGFIX] fixed build error

--- Runtime/Components/ClockDigital.cs
+++ Runtime/Components/ClockDigital.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using UnityEditor;
+using System;
 using UnityEngine;
 using UnityEngine.Events;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 
 
 [ExecuteInEditMode]
 public class ClockDigital : MonoBehaviour {
+  #region Members
 
-  [System.Serializable]
-  public class TimeTextEvent : UnityEvent<string> { }
-  [SerializeField] private TimeTextEvent tick;
+  [SerializeField]
+  private TimeTextEvent tick;
 
+  #endregion
 
+  #region Methods and others
 
-  private void Awake() {
-    EditorApplication.update += Update;
-  }
+#if UNITY_EDITOR
+  private void Awake() => EditorApplication.update += Update;
+#endif
 
 
 
@@ -31,4 +35,14 @@ public class ClockDigital : MonoBehaviour {
 
 
   private string LeadingZero(int n) => n.ToString().PadLeft(2, '0');
+
+  #endregion
+
+  #region Nested Types
+
+  [Serializable]
+  public class TimeTextEvent : UnityEvent<string> { }
+
+  #endregion
 }
+
